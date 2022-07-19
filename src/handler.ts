65130aaa8402e49a61f4804722d41d3aed31ee28
Adam <adam@haglund.dev>
2022-07-19 13:34:50 +0200

    add param validation

--- src/handler.ts
+++ src/handler.ts
@@ -5,13 +5,15 @@ import { badRequest } from "@worker-tools/response-creators"
 import { Dota } from "./dota"
 import { json } from "./utils"
 
+const validIdsRegex = /^(?:\d+,?){0,5}\d+$/
+
 export const handler: ExportedHandlerFetchHandler<Env> = async (request, env) => {
   const idsParam = new URL(request.url).searchParams.get("ids")
-  if (idsParam == null) {
-    return badRequest("Missing ids")
+  if (idsParam == null || !validIdsRegex.test(idsParam)) {
+    return badRequest("Missing or invalid ids parameter")
   }
 
-  const ids = idsParam.split(",").map(Number)
+  const ids = idsParam.split(",").filter(Boolean).map(Number)
   const data = await Dota.getTeamsData(env, ids)
 
   return json(data)
