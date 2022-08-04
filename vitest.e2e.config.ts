bbffad94a761253a090b9b1b0d150523698a52da
Adam <adam@haglund.dev>
2022-08-04 14:35:29 +0200

    add e2e test

---
+++ vitest.e2e.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vitest/config"
+import { config } from "dotenv"
+
+config()
+
+export default defineConfig({
+  test: {
+    env: {
+      API_BASE: process.env.API_BASE!,
+    },
+    include: ["e2e/**/*.test.ts"],
+    setupFiles: "vitest.setup.ts",
+    reporters: "verbose",
+  },
+})
