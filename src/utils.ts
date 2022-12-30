6c07063a9d269a3a30cf7d8e7f1f4e86916940e8
Adam <adam@haglund.dev>
2022-12-30 02:13:14 +0100

    improve function

--- src/utils.ts
+++ src/utils.ts
@@ -32,8 +32,8 @@ export const json = <T = unknown>(data: T, init?: RequestInitExStatus) =>
         },
   )
 
-export const getCountry = (request: IRequest) => {
-  if (request.cf != null && "country" in request.cf) {
+export const getCountry = (request: IRequest): string => {
+  if (request.cf != null && request.cf.country != null) {
     return request.cf.country
   }
 
