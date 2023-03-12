2836a333c7679f2c2b424a34a25c7790efd87092
Adam <adam@haglund.dev>
2023-03-12 17:37:30 +0100

    add cors
    closes #186

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,25 @@
+import { createCors } from "itty-cors"
 import { Router } from "itty-router"
 
-import { notFound, temporaryRedirect } from "@worker-tools/response-creators"
+import {
+  notFound,
+  temporaryRedirect,
+  internalServerError,
+} from "@worker-tools/response-creators"
 
 import { notifier } from "./notify"
 import { v1Router } from "./routes/v1"
 import { CustomRouter } from "./types"
 
 const router = Router() as CustomRouter
+
+const { preflight, corsify } = createCors({
+  methods: ["GET"],
+  origins: ["*"],
+  maxAge: 7200,
+})
+
+router.all("*", preflight as any)
 
 router.all("/v1/*", v1Router.handle)
 
@@ -15,7 +28,14 @@ router.get("/", () => temporaryRedirect("https://github.com/BeeeQueue/dota-match
 router.all("*", () => notFound())
 
 const worker: ExportedHandler<Env> = {
-  fetch: router.handle as never,
+  fetch: (...args) =>
+    router
+      .handle(...args)
+      .catch((error: Error) => {
+        console.error(error)
+        return internalServerError("Something went wrong!")
+      })
+      .then(corsify),
   scheduled: notifier,
 }
 
