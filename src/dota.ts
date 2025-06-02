6b9d4776209d33d636a64522d380752883049890
Adam <adam@haglund.dev>
2025-06-02 18:06:25 +0200

    dont do stale-while-revalidate

--- src/dota.ts
+++ src/dota.ts
@@ -221,12 +221,7 @@ export const getTeams =
   async (country: string): Promise<string[]> => {
     const lastFetched = Number((await env.META.get(MetaKey.TEAMS_LAST_FETCHED)) ?? -1)
     console.log(`Teams were last fetched at ${lastFetched}`)
-    if (lastFetched !== -1) {
-      if (Date.now() > lastFetched + ms("12h")) {
-        console.log(`Teams are stale, refreshing...`)
-        void fetchAndCacheTeams(env, country)
-      }
-
+    if (lastFetched !== -1 && Date.now() <= lastFetched + ms("12h")) {
       console.log(`Retrieving teams from DB...`)
       const result = await getTeamsFromDb()
       return result.rows.map(({ name }) => name)
