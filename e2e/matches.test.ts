4e283ba43ca7b16e56f0ba3739b7152cdedde80c
Adam <adam@haglund.dev>
2025-06-04 03:59:16 +0200

    re-enable e2e test

--- e2e/matches.test.ts
+++ e2e/matches.test.ts
@@ -1,18 +1,19 @@
-import { mande } from "mande"
-import { expect } from "vitest"
+import xior from "xior"
+import { expect, it } from "vitest"
 
 import type { Match } from "../src/dota.ts"
 
 it("no field is missing from all matches", async () => {
-  const matches = await mande(import.meta.env.API_BASE!).get<Match[]>("/v1/matches", {
+  const response = await xior.get<Match[]>(`${process.env.API_BASE!}/v1/matches`, {
     headers: {
       "User-Agent": "github.com/BeeeQueue/dota-matches-api (e2e)",
     },
   })
+  if (response.data.length < 3) return
 
   // Amount of times a field appears in a match
   const fieldAmounts: Record<string, number> = {}
-  for (const match of matches) {
+  for (const match of response.data) {
     for (const field of Object.keys(match)) {
       if (match[field as keyof typeof match] != null) {
         fieldAmounts[field] ??= 0
--- vitest.e2e.config.ts
+++ vitest.e2e.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vitest/config"
 export default defineConfig({
   test: {
     env: {
-      API_BASE: import.meta.env.API_BASE!,
+      API_BASE: process.env.API_BASE!,
     },
     include: ["e2e/**/*.test.ts"],
     reporters: "verbose",
