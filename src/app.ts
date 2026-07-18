151c9f17d77925839d48be1fd71733048b0c2862
bq <adam@haglund.dev>
2026-07-18 12:23:29 +0200

    add back env registration for db0

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,18 @@
 import { H3, handleCors, redirect } from "h3"
 
+import { registerEnv } from "./db0-dialect/d1-register"
 import { v1Router } from "./routes/v1/index.ts"
+import { getEnv } from "./utils"
 
 export const createApp = () => {
   const app = new H3()
+
+  let isRegistered = false
+  app.use((event) => {
+    if (isRegistered) return
+    registerEnv(getEnv(event))
+    isRegistered = true
+  })
 
   app.all("/", (event) => {
     const result = handleCors(event, {
